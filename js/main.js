2a8e1241f05b08bb17dfe6933a9e0c9394cfeecf
Luan Rocha <kuro.winchester@gmail.com>
2023-11-21 21:28:16 -0300

    getting pokemon name by api

--- js/main.js
+++ js/main.js
@@ -4,24 +4,29 @@ const offset = 0
 const limit = 20
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jasonBody) => console.log(jasonBody))
-    .catch((error) => console.error(error))
-
-for(i = 0; i < 10; i++){
-    pokemonList.innerHTML += `<li class="pokemon">
+function pokemonToLi(pokemon) {
+    return `<li class="pokemon">
         <span class="number"></span>
-        <span class="name">Bulbasaur</span>
+        <span class="name">${pokemon.name}</span>
     
         <div class="detail">
             <ol class="types">
                 <li class="type">Grass</li>
                 <li class="type">Poison</li>
             </ol>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="Bulbasaur">
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
         </div>
         </li>
     `
-    
 }
+
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokeList) => {
+        for(let i = 0; i < pokeList.length; i++) {
+            const pokemon = pokeList[i];
+            pokemonList.innerHTML += pokemonToLi(pokemon)
+        }
+    })
+    .catch((error) => console.error(error))
