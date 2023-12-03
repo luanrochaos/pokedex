e5619de8115400a025bd6ae0843c02e30ee23f25
Luan Rocha <kuro.winchester@gmail.com>
2023-12-02 22:22:12 -0300

    card color by type

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 const pokemonList = document.querySelector(".pokemon-list")
 
 function convertPokemonToLi(pokemon) {
-    return `<li class="pokemon">
+    return `<li class="pokemon ${pokemon.type}">
         <span class="number">#${pokemon.number}</span>
         <span class="name">${pokemon.name}</span>   
         <div class="detail">
             <ol class="types">
-                ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
+                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
             </ol>
             <img src="${pokemon.img}" alt="${pokemon.name}">
         </div>
